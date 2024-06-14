833ba65731158edf67d1288cac5ecf8b5d67afb0
jayalloyd <jayaranilloyd1983@gmail.com>
2024-06-14 13:49:01 +0530

    implemented retrieval from databse

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollno,name,marks) values(101,'lloyd',85);
+insert into student (rollno,name,marks) values(102,'meenu',98);
+insert into student(rollno,name,marks) values(103,'rooney',99);
